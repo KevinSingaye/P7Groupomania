a76d66ae53048415155fd9e82297ce8437885599
KevinSingaye <75949789+KevinSingaye@users.noreply.github.com>
2022-06-24 12:33:55 +0200

    redirection login vers register

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { UtilisateurService } from 'src/app/services/utilisateur.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  identifiant: string = '';
+  motDePasse: string = '';
+
+  constructor(private service: UtilisateurService, private cookie: CookieService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onRegister(): void {
+    this.service.Register(this.identifiant, this.motDePasse).subscribe((result) => {
+      sessionStorage.setItem('userId', result.userId)
+      this.cookie.set('token', result.token)
+      this.router.navigate([''])
+
+    }
+      , (error) => {
+        console.log('error', error);
+      }
+    )
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- frontend/src/app/services/utilisateur.service.ts
+++ frontend/src/app/services/utilisateur.service.ts
@@ -10,12 +10,18 @@ import { LoginComponent } from '../components/login/login.component';
 export class UtilisateurService {
 
   path = 'http://localhost:3000/api/auth'
+
   constructor(private http: HttpClient) {
 
   }
   Login(identifiant: string, password: string): Observable<any> {
     const body = { email: identifiant, password }
     return this.http.post(this.path + '/login', body);
+
+  }
+  Register(identifiant: string, password: string): Observable<any> {
+    const body = { email: identifiant, password }
+    return this.http.post(this.path + '/signup', body);
 
   }
 }
