d7959d7f04a931cc960c998055d51bf4d1e78d41
KevinSingaye <75949789+KevinSingaye@users.noreply.github.com>
2022-07-01 03:24:51 +0200

    ajout front

---
+++ frontend/src/app/components/main/main.component.js
@@ -0,0 +1,67 @@
+// SIDEBAR
+
+const menuItems = document.querySelectorAll('.menu-item');
+
+// MESSAGES 
+const messagesNotifications = document.querySelector('#messages-notification');
+
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+// ================= SIDEBAR ==========
+// remove active class from all menu items 
+
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if (item.id != 'notifications') {
+            document.querySelector('.notifications-popup').
+            style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').
+            style.display = 'block';
+            document.querySelector('#notifications . notification-count').style.display = 'none';
+        }
+    })
+
+})
+
+// ====  MESSAGES======
+
+
+//searches Chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if (name.indexOf(val) != -1) {
+            user.style.display = 'flex';
+        } else {
+            user.style.display = 'none';
+        }
+    })
+}
+
+// Search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+
+// highlight messages card when messages menu item is clicked
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+//
